d3d58167cf75a3e689287b794108decbd9041c9e
Mauricio Soria <mauricioutn2017@gmail.com>
2023-06-01 11:58:51 -0300

    Estructura del proyecto con form

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Formulario from './components/Formulario'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
+function App() {
+
+  return (
+    <>
+    <Formulario></Formulario>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
